212871d4320440be286fecd62009745ad7b7ebef
Ellisande <justin.dragos@gmail.com>
2018-03-25 15:42:25 -0700

    Add a display component for the average ticket time.

---
+++ src/AverageNumberTime.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import AverageNumberTimeProvider from './AverageNumberTimeProvider';
+
+const AverageNumberTime: React.SFC<{}> = () => (
+    <AverageNumberTimeProvider path="/minefaire">
+        {averageTime => <div>The average ticket takes {averageTime.humanize()}</div>}
+    </AverageNumberTimeProvider>
+);
+
+export default AverageNumberTime;
