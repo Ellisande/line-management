0fe5090028bc4e21b5cf2d5ea2edb6584ba91e24
Ellisande <justin.dragos@gmail.com>
2018-04-26 22:34:19 -0700

    UX Enhancements

--- src/containers/Manage.tsx
+++ src/containers/Manage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as moment from "moment";
 import { Route } from "react-router-dom";
+import { StyleSheet, css } from "aphrodite";
 
 import CurrentQueuerProvider from "../providers/CurrentQueuerProvider";
 import MarkServedProvider from "../providers/MarkServedProvider";
@@ -13,6 +14,7 @@ import AcceptingNumbersUpdater from "../providers/AcceptingNumbersUpdater";
 import EverythingRemover from "../providers/EverythingRemover";
 import Terminal from "./Terminal";
 import Dashboard from "./Dashboard";
+import WaitProvider from "../providers/WaitProvider";
 
 interface AcceptNumbersProps {
   onStartAccepting: () => void;
@@ -66,9 +68,39 @@ interface Props {
   match: { url: string };
 }
 
+const styles = StyleSheet.create({
+  bigNumber: {
+    fontSize: "40px",
+    fontWeight: "bold",
+    display: "flex",
+    justifyContent: "center"
+  },
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    ":nth-child(n) > * + *": {
+      marginTop: "10px"
+    }
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "center",
+    ":nth-child(n) > * + *": {
+      marginLeft: "7px"
+    }
+  },
+  subtle: {
+    fontSize: "10px"
+  },
+  leaving: {
+    fontSize: "8px"
+  }
+});
+
 const Manage: React.SFC<Props> = ({ match }) => {
   return (
-    <div>
+    <div className={css(styles.layout)}>
       <CurrentQueuerProvider>
         {(currentQueuer, id) =>
           currentQueuer && id ? (
@@ -76,11 +108,19 @@ const Manage: React.SFC<Props> = ({ match }) => {
               {markCurrentComplete => (
                 <CallNextNumberProvider>
                   {pullNext => (
-                    <MarkDone
-                      markAsDone={markCurrentComplete}
-                      pullNextNumber={pullNext}
-                      currentNumber={currentQueuer.number}
-                    />
+                    <div>
+                      <div className={css(styles.bigNumber)}>
+                        {currentQueuer.number}
+                      </div>
+                      <div className={css(styles.actions)}>
+                        <MarkDone
+                          markAsDone={markCurrentComplete}
+                          pullNextNumber={pullNext}
+                          currentNumber={currentQueuer.number}
+                        />
+                        <SkipNumber />
+                      </div>
+                    </div>
                   )}
                 </CallNextNumberProvider>
               )}
@@ -90,12 +130,30 @@ const Manage: React.SFC<Props> = ({ match }) => {
           )
         }
       </CurrentQueuerProvider>
-      <AverageNumberTime />
-      <SkipNumber />
+      <div className="wait times">
+        <AverageNumberTime />
+        <WaitProvider>
+          {wait => (
+            <div>
+              <div>Current wait is {wait.humanize()}</div>
+              <div>
+                Estimated time to done{" "}
+                {moment()
+                  .add(wait)
+                  .format("MM-DD hh:mma")}
+              </div>
+            </div>
+          )}
+        </WaitProvider>
+      </div>
       <StartStopNumbers />
       <ResetNumbers onResetNumbers={() => undefined} />
-      <Route path={`${match.url}/terminal`} component={Terminal} />
-      <Route path={`${match.url}/dashboard`} component={Dashboard} />
+      <Route path={`${match.url}/terminal`} exact={true} component={Terminal} />
+      <Route
+        path={`${match.url}/dashboard`}
+        exact={true}
+        component={Dashboard}
+      />
     </div>
   );
 };
