4b4a31e3f4c83050b57265a0305a70b08885655a
Ellisande <justin.dragos@gmail.com>
2018-05-03 20:43:44 -0700

    Added in content routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { StyleSheet, css } from "aphrodite";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { FirebaseProvider, RootRef } from "fire-fetch";
 
 import Manage from "./containers/Manage";
 import config from "./firebaseConfig";
 import { User } from "./containers/User";
-
-const linkStyles = StyleSheet.create({
-  linkList: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "flex-end",
-    paddingBottom: "15px"
-  }
-});
+import { Navigation } from "./containers/Navigation";
+import Terminal from "./containers/Terminal";
+import Dashboard from "./containers/Dashboard";
 
 class App extends React.Component {
   render() {
@@ -22,29 +15,37 @@ class App extends React.Component {
       <FirebaseProvider config={config}>
         <Router>
           <div>
-            <Route
-              path="/:aLine"
-              render={({ match }) => (
-                <RootRef path={`/${match.params.aLine}`}>
+            <Route path="/:line_name/">
+              {({ match }) => (
+                <RootRef path={`/${match.params.line_name}`}>
                   <div>
-                    <Route
-                      path={`${match.url}`}
-                      render={() => (
-                        <div className={css(linkStyles.linkList)}>
-                          <Link to={`${match.url}/manage`}>Manage Line</Link>
-                        </div>
-                      )}
-                    />
-                    <Route
-                      path={`${match.url}`}
-                      exact={true}
-                      component={User}
-                    />
-                    <Route path={`${match.url}/manage`} component={Manage} />
+                    <Navigation />
+                    <Switch>
+                      <Route
+                        path={`${match.url}/manage`}
+                        exact={true}
+                        component={Manage}
+                      />
+                      <Route
+                        path={`${match.url}/terminal`}
+                        exact={true}
+                        component={Terminal}
+                      />
+                      <Route
+                        path={`${match.url}/dashboard`}
+                        exact={true}
+                        component={Dashboard}
+                      />
+                      <Route
+                        path={`${match.url}`}
+                        exact={true}
+                        component={User}
+                      />
+                    </Switch>
                   </div>
                 </RootRef>
               )}
-            />
+            </Route>
           </div>
         </Router>
       </FirebaseProvider>
