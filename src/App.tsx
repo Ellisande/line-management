85e9458f10bc10ecee7f0fdc44b5d3bb48829fbe
Ellisande <justin.dragos@gmail.com>
2018-05-05 12:35:05 -0700

    Added application background color

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ import { User } from "./containers/User";
 import { Navigation } from "./containers/Navigation";
 import Terminal from "./containers/Terminal";
 import Dashboard from "./containers/Dashboard";
+import { ThemeProvider, Style } from "./styles/ThemeProvider";
+import { defaultTheme, Theme } from "./styles/theme";
+import { css } from "aphrodite";
+
+const styleBuilder = ({ colors: { background } }: Theme) => ({
+  pageLayout: {
+    backgroundColor: background,
+    height: "100vh"
+  }
+});
 
 class App extends React.Component {
   render() {
@@ -18,31 +28,37 @@ class App extends React.Component {
             <Route path="/:line_name/">
               {({ match }) => (
                 <RootRef path={`/${match.params.line_name}`}>
-                  <div>
-                    <Navigation />
-                    <Switch>
-                      <Route
-                        path={`${match.url}/manage`}
-                        exact={true}
-                        component={Manage}
-                      />
-                      <Route
-                        path={`${match.url}/terminal`}
-                        exact={true}
-                        component={Terminal}
-                      />
-                      <Route
-                        path={`${match.url}/dashboard`}
-                        exact={true}
-                        component={Dashboard}
-                      />
-                      <Route
-                        path={`${match.url}`}
-                        exact={true}
-                        component={User}
-                      />
-                    </Switch>
-                  </div>
+                  <ThemeProvider value={defaultTheme}>
+                    <Style buildStyles={styleBuilder}>
+                      {styles => (
+                        <div className={css(styles.pageLayout)}>
+                          <Navigation />
+                          <Switch>
+                            <Route
+                              path={`${match.url}/manage`}
+                              exact={true}
+                              component={Manage}
+                            />
+                            <Route
+                              path={`${match.url}/terminal`}
+                              exact={true}
+                              component={Terminal}
+                            />
+                            <Route
+                              path={`${match.url}/dashboard`}
+                              exact={true}
+                              component={Dashboard}
+                            />
+                            <Route
+                              path={`${match.url}`}
+                              exact={true}
+                              component={User}
+                            />
+                          </Switch>
+                        </div>
+                      )}
+                    </Style>
+                  </ThemeProvider>
                 </RootRef>
               )}
             </Route>
