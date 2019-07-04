93f788655888351c1ee55acdc0e1c4fd809af50b
Ellisande <justin.dragos@gmail.com>
2019-07-03 21:28:51 -0700

    Add reverse lookup maps for string enums

--- src/Line.ts
+++ src/Line.ts
@@ -8,10 +8,25 @@ export enum GroupPreference {
   FIVE = "Five"
 }
 
+export const groupPreferenceMap: {[key: string]: GroupPreference} = {
+  "One": GroupPreference.ONE,
+  "Two": GroupPreference.TWO,
+  "Three": GroupPreference.THREE,
+  "Four": GroupPreference.FOUR,
+  "Five": GroupPreference.FIVE,
+}
+
+
 export enum SkipPreference {
   REMOVE = "Remove",
   NEXT = "Next",
   LOW_PRIORITY = "Low Priority"
+}
+
+export const skipPreferenceMap: {[key: string]: SkipPreference} = {
+  "Remove": SkipPreference.REMOVE,
+  "Next": SkipPreference.NEXT,
+  "Low Priority": SkipPreference.LOW_PRIORITY
 }
 
 export enum CallPreference {
@@ -19,16 +34,30 @@ export enum CallPreference {
   PICK_NUMBERS = "Pick Numbers"
 }
 
+export const callPreferenceMap: {[key: string]: CallPreference} = {
+  "Ordered": CallPreference.ORDERED,
+  "Pick Numbers": CallPreference.PICK_NUMBERS
+}
+
 export enum DurationPreference {
   NO_MAXIMUM_DURATION = "No Maximum Duration"
 }
+
+export const durationPreferenceMap: {[key: string]: DurationPreference} = {
+  "No Maximum Duration": DurationPreference.NO_MAXIMUM_DURATION
+}
+
 export enum CapacityPreference {
   NO_MAXIMUM_CAPACITY = "No Maximum Capacity"
 }
 
+export const capacityPreferenceMap: {[key: string]: CapacityPreference} = {
+  "No Maximum Capacity": CapacityPreference.NO_MAXIMUM_CAPACITY
+}
+
 export interface Preferences {
   groupPreference: GroupPreference;
-  skipPrefences: SkipPreference;
+  skipPreference: SkipPreference;
   callPreference: CallPreference;
   maxDuration: moment.Duration | DurationPreference;
   maximumCapacity: number | CapacityPreference;
