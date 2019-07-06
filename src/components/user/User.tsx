0f3c58a7b5fec18f0c26ca38bf4bb86cf55a2539
Ellisande <justin.dragos@gmail.com>
2019-07-06 00:19:42 -0700

    Add an empty state if the line does not exist.

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -4,12 +4,28 @@ import { WithoutNumber } from "./WithoutNumber";
 import { WithNumber } from "./WithNumber";
 import { useQueuer } from "../../hooks/useQueuer";
 import { useLineName } from "../../context/lineNameContext";
-import { createContext, useState, useCallback, useEffect } from "react";
+import { createContext, useState, useCallback, useEffect, Fragment } from "react";
+import { useLineExists } from "../../hooks/useLineExists";
+import { Theme } from "../../theme/theme";
+import { useStyle } from "../../theme/useStyle";
 
 const LocalQueuerIdContext = createContext<string | undefined>(undefined);
 
+const styleBuilder = ({ colors, buttons, font }: Theme) => ({
+  emptyState: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '40vh'
+  }
+});
+
+
 export const User = () => {
+  const styles = useStyle(styleBuilder);
   const lineName = useLineName();
+  const lineExists = useLineExists();
   const storageKey = `${lineName}:number:`;
   const [localQueuerId, setLocalQueuerId] = useState(
     localStorage.getItem(storageKey) || undefined
@@ -25,15 +41,23 @@ export const User = () => {
   }, [lineName, localQueuerId]);
   const localQueuer = useQueuer(localQueuerId);
   return (
-    <LocalQueuerIdContext.Provider value={localQueuerId}>
-      {localQueuer && !localQueuer.leftAt ? (
-        <WithNumber
-          localQueuer={localQueuer}
-          clearLocalQueuerId={clearLocalQueuerId}
-        />
-      ) : (
-        <WithoutNumber setLocalQueuerId={setLocalQueuerId} />
-      )}
-    </LocalQueuerIdContext.Provider>
+    <Fragment>
+      {lineExists && <LocalQueuerIdContext.Provider value={localQueuerId}>
+        {localQueuer && !localQueuer.leftAt ? (
+          <WithNumber
+            localQueuer={localQueuer}
+            clearLocalQueuerId={clearLocalQueuerId}
+          />
+        ) : (
+            <WithoutNumber setLocalQueuerId={setLocalQueuerId} />
+          )}
+      </LocalQueuerIdContext.Provider>}
+      {!lineExists &&
+        <div css={styles.emptyState}>
+          <div>This line does not exist.</div>
+          <div>Check your url or re-scan the QR code.</div>
+        </div>
+      }
+    </Fragment>
   );
 };
