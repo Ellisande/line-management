db7e74cf3e8656b628c957ea7c34d85253b60b43
Ellisande <justin.dragos@gmail.com>
2019-08-02 22:22:00 -0700

    Added permissions for terminal and manage

--- src/Permission.ts
+++ src/Permission.ts
@@ -1,6 +1,8 @@
 export enum PermissionEnum {
   LINE_CREATE = "line_create",
-  SUPER_ADMIN = "super_admin"
+  SUPER_ADMIN = "super_admin",
+  TERMINAL = "terminal",
+  MANAGE = "manage"
 }
 
 export interface Permission {
