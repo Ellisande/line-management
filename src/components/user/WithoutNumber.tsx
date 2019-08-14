97a4aad0f08dee431fab40184183f910e1ad418e
Ellisande <justin.dragos@gmail.com>
2019-08-13 21:48:05 -0700

    Allow use to log in on the take a number page

--- src/components/user/WithoutNumber.tsx
+++ src/components/user/WithoutNumber.tsx
@@ -9,8 +9,10 @@ import { useAverageServiceTime } from "../../hooks/useAverageServiceTime";
 import { EstimatedWait } from "../EstimatedWait";
 import { NumberDispenser } from "../NumberDispenser";
 import { NotAccepting } from "../NotAccepting";
+import { useSignIn } from "../../hooks/useSignIn";
+import { useAuthenticated } from "../../hooks/useAuthenticated";
 
-const styleBuilder = ({ colors: { text } }: Theme) => ({
+const styleBuilder = ({ colors: { text, button }, buttons, font }: Theme) => ({
   layout: {
     display: "flex",
     minHeight: "80vh",
@@ -18,8 +20,12 @@ const styleBuilder = ({ colors: { text } }: Theme) => ({
     justifyContent: "center",
     alignItems: "center",
     padding: "1rem",
-    ":nth-child(n) > * + *": {
+    ":nth-child(n) > :first-child": {
+      marginBottom: "10rem",
       marginTop: "10rem"
+    },
+    ":nth-child(n) > * + *": {
+      marginTop: "5rem"
     }
   },
   text: {
@@ -34,6 +40,21 @@ const styleBuilder = ({ colors: { text } }: Theme) => ({
     ":nth-child(n) > * + *": {
       marginTop: "3rem"
     }
+  },
+  loginButton: {
+    ...buttons.borderOptions,
+    ...buttons.paddingOptions,
+    backgroundColor: button.secondary,
+    color: text.primary,
+    fontSize: font.size.large
+  },
+  loginLayout: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: "15rem"
+  },
+  noMargin: {
+    margin: "16px"
   }
 });
 
@@ -47,6 +68,8 @@ export const WithoutNumber: React.SFC<Props> = ({ setLocalQueuerId }) => {
   const lineCount = useLineCount(undefined);
   const averageServiceTime = useAverageServiceTime();
   const waitTime = moment.duration(lineCount * averageServiceTime);
+  const login = useSignIn();
+  const loggedIn = useAuthenticated();
 
   if (!acceptingNumbers) {
     return <NotAccepting />;
@@ -54,6 +77,15 @@ export const WithoutNumber: React.SFC<Props> = ({ setLocalQueuerId }) => {
   return (
     <div css={style.layout}>
       <NumberDispenser onDispense={setLocalQueuerId} />
+      {!loggedIn && (
+        <div css={style.loginLayout}>
+          <p css={style.noMargin}>Already have a number?</p>
+          <button onClick={login} css={style.loginButton}>
+            Log in
+          </button>
+        </div>
+      )}
+
       <div css={style.wait}>
         <EstimatedWait waitTime={waitTime} className={style.text} />
       </div>
