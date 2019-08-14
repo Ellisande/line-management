97465b43a2d730b8543d8e7ba171479e80e7f9b1
Ellisande <justin.dragos@gmail.com>
2019-08-13 21:48:57 -0700

    Fetch user's number using user id if logged in

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -2,30 +2,37 @@
 import { jsx } from "@emotion/core";
 import { WithoutNumber } from "./WithoutNumber";
 import { WithNumber } from "./WithNumber";
-import { useQueuer } from "../../hooks/useQueuer";
+import { useQueuer, useLoggedInQueuer } from "../../hooks/useQueuer";
 import { useLineName } from "../../context/lineNameContext";
-import { createContext, useState, useCallback, useEffect, Fragment } from "react";
+import {
+  createContext,
+  useState,
+  useCallback,
+  useEffect,
+  Fragment
+} from "react";
 import { useLineExists } from "../../hooks/useLineExists";
 import { Theme } from "../../theme/theme";
 import { useStyle } from "../../theme/useStyle";
+import { useAuthenticated } from "../../hooks/useAuthenticated";
 
 const LocalQueuerIdContext = createContext<string | undefined>(undefined);
 
 const styleBuilder = ({ colors, buttons, font }: Theme) => ({
   emptyState: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '40vh'
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "40vh"
   }
 });
-
 
 export const User = () => {
   const styles = useStyle(styleBuilder);
   const lineName = useLineName();
   const lineExists = useLineExists();
+  const userId = useAuthenticated();
   const storageKey = `${lineName}:number:`;
   const [localQueuerId, setLocalQueuerId] = useState(
     localStorage.getItem(storageKey) || undefined
@@ -40,24 +47,28 @@ export const User = () => {
     return localStorage.removeItem(storageKey);
   }, [lineName, localQueuerId]);
   const localQueuer = useQueuer(localQueuerId);
+  const loggedInQueuer = useLoggedInQueuer(userId);
+  const currentQueuer = loggedInQueuer || localQueuer;
   return (
     <Fragment>
-      {lineExists && <LocalQueuerIdContext.Provider value={localQueuerId}>
-        {localQueuer && !localQueuer.leftAt ? (
-          <WithNumber
-            localQueuer={localQueuer}
-            clearLocalQueuerId={clearLocalQueuerId}
-          />
-        ) : (
+      {lineExists && (
+        <LocalQueuerIdContext.Provider value={localQueuerId}>
+          {currentQueuer && !currentQueuer.leftAt ? (
+            <WithNumber
+              localQueuer={currentQueuer}
+              clearLocalQueuerId={clearLocalQueuerId}
+            />
+          ) : (
             <WithoutNumber setLocalQueuerId={setLocalQueuerId} />
           )}
-      </LocalQueuerIdContext.Provider>}
-      {!lineExists &&
+        </LocalQueuerIdContext.Provider>
+      )}
+      {!lineExists && (
         <div css={styles.emptyState}>
           <div>This line does not exist.</div>
           <div>Check your url or re-scan the QR code.</div>
         </div>
-      }
+      )}
     </Fragment>
   );
 };
